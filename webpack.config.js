8f3b3e793909d61961d3cabe64e4fcc7ed877e2a
akarmanov <alexandrk_@hotmail.com>
2018-10-30 12:57:57 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src/main/javascript/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'src/main/resources/javascript/apps/'),
+        filename: 'vueApp.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
